b28468f663660982b15af63537181f8488db444c
Benny <blaze349@protonmail.com>
2017-04-06 14:28:44 +0000

    Added some methods for Vector class"

--- src/util/vector.js
+++ src/util/vector.js
@@ -1,10 +1,22 @@
 class Vector {
-    constructor() {
-        
+    constructor(x, y) {
+        this.x = x
+        this.y = y
     }
-    x;
-    y;
-    length;
+    
+    length() {
+        //I wish my linter supported ES6
+        return Math.sqrt(x ** 2 + y ** 2)
+    }
+    
+    static add(vec1, vec2) {
+        //The order is not important here
+        return new Vector(vec1.x + vec2.x, vec1.y, vec2.y)
+    }
+    static sub(vec1, vec2) {
+        //The order is very important here The one to subtract off off goes first
+        return new Vector(vec1.x - vec2.x, vec1.y - vec2.y)
+    }
 }
 
 module.exports = Vector
